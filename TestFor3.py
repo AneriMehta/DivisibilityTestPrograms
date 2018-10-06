4e8b294922378a87afe2c42e948856600d8a955d
Satyajit Das <41534259+satyajit08082001@users.noreply.github.com>
2018-10-07 01:18:50 +0530

    updated.py

--- TestFor3.py
+++ TestFor3.py
@@ -1,20 +1,20 @@
-def test_for_3(quotient):
-    """Divisibility test to check if the quotient is 
-    a multiple of 3, without using the modulo operator.
-    This can be done, by checking if the digit sum, 
-    is 3, 6, or 9, once it gets to be a single digit."""
-    digit_sum = int(quotient)
-    while digit_sum > 9:
-        digits = str(digit_sum)
-        total = 0
-        for digit in digits:
-            total += int(digit)
-        digit_sum = total
-    if digit_sum in [0, 3, 6, 9]:
-        return True
-    else:
-        return False
-
-# Test to check that the function behaves properly
-for i in range(100):
-    print("{} divisible by 3 {}".format(i, test_for_3(i)))
+def check(st) : 
+  
+    # Compute sum of digits 
+    n = len(st) 
+    digitSum = 0
+      
+    for i in range(0,n) : 
+        digitSum = digitSum + (int)(st[i]) 
+  
+    # Check if sum of digits 
+    # is divisible by 9. 
+    return (digitSum % 9 == 0) 
+  
+# Driver code 
+st = "99333"
+  
+if(check(st)) : 
+    print("Yes") 
+else :  
+    print("No")
