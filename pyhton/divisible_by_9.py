266f9a2c1f09490d06d0ebf4676b5ff71132c197
sauravpurva <ced16i029@iiitdm.ac.in>
2018-10-07 00:30:18 +0530

    Divisible by 9

---
+++ pyhton/divisible_by_9.py
@@ -0,0 +1,21 @@
+#divisibility by 9
+def check(st) : 
+  
+    # Compute sum of digits 
+    n = len(st) 
+    digitSum = 0
+      
+    for i in range(0,n) : 
+        digitSum = digitSum + (int)(st[i]) 
+  
+    # Check if sum of digits 
+    # is divisible by 9. 
+    return (digitSum % 9 == 0) 
+  
+# Driver code 
+st = "99333"
+  
+if(check(st)) : 
+    print("Yes") 
+else :  
+    print("No")
