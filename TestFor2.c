7a9e0f050e0ec93fdaf29ee624c9fb3125f1e5b9
BansiJoshi <bkjoshi14297@gmail.com>
2018-10-06 18:18:44 +0530

    Divisible by 2

---
+++ TestFor2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void main()
+{
+    int i ;
+    printf("Enter an integer:");
+    scanf("%d",&i);
+    if(i%2 == 0)
+    printf("Number is divisible by 2");
+    else
+    printf("Number is not divisible by 2\n");
+}
