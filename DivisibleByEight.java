67d57bd3f08f72d377b5000b32972aa9e868f576
niralip1397@gmail.com <sjgohil9848@gmail.com>
2018-10-06 21:08:25 +0530

    Divisible by 8

---
+++ DivisibleByEight.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+class DivisibleByEight
+{
+    public static void main(String args[])
+    {
+        Scanner x =new Scanner(System.in);
+        int num=0;
+        System.out.print("Enter a number..:");
+        int n = x.nextInt();
+        int n2 = n;
+        int temp =(int) n2 / 8;
+        for(int i=1;i<=temp;i++)
+        {
+            num=num+8;
+        }   
+        if(num == n2)
+        {
+            System.out.println(n2 +" is divisible by 8!!");
+        }
+        else
+        {
+            System.out.println(n2 +" is Not divisible by 8!!");
+        }
+    }
+}
