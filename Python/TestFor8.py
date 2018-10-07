3b6ee47024c5802f944eac23d25c503697bbe81d
Vytas <43892711+weedska@users.noreply.github.com>
2018-10-07 20:33:29 +0200

    Update TestFor8.py

--- Python/TestFor8.py
+++ Python/TestFor8.py
@@ -1,4 +1,7 @@
 def testfor8(imput):
+    if type(imput) is not int:
+        print('Wrong imput, imput is not divisible by 8')
+        return False
     if imput % 8 == 0:
         print(str(imput), 'is divisible by 8')
     else:
