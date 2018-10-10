fb69416974743acf93294ea247dee24efce32f4b
Vytas <43892711+weedska@users.noreply.github.com>
2018-10-10 20:21:39 +0200

    Update TestFor8.py
    Changed code, now without modulo operator.

--- Python/TestFor8.py
+++ Python/TestFor8.py
@@ -2,7 +2,7 @@ def testfor8(imput):
     if type(imput) is not int:
         print('Wrong imput, imput is not divisible by 8')
         return False
-    if imput % 8 == 0:
+    if (imput / 8) - int(imput / 8) == 0:
         print(str(imput), 'is divisible by 8')
     else:
         print(str(imput), 'is not divisible by 8')
