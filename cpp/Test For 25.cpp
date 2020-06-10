09dd575c2ab1e314e0d5ee6273cd8e329a0382c8
Alka Trivedi <iit2019055@iiita.ac.in>
2020-06-10 20:52:38 +0530

    Test to check divisibility by 25

---
+++ cpp/Test For 25.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int num;
+	cin>>num;
+	if(num%25==0)
+	cout<<"Number is divisible by 25"<<endl;
+	else
+	cout<<"Number is not divisible by 25"<<endl;
+	return 0;
+}
